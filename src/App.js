64ff0f7b8d474da473f64588f3c51c21c186ff66
Faithulla <fayzullashakirov01@gmail.com>
2021-07-17 02:13:56 +0500

    Added scss files and some components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import HomePage from './Components/Home/HomePage';
+import HomePage from './Components/home/HomePage';
 
  class App extends Component {
   constructor(props) {
