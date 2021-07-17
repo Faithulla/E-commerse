93120c0a3c99471ee2f56ecee68d15356334f314
Meepo <Muniss138@gmail.com>
2021-07-17 15:39:11 +0500

    changed

--- src/Components/Directory/Directory.jsx
+++ src/Components/Directory/Directory.jsx
@@ -9,7 +9,7 @@ class Directory extends React.Component {
         this.state = { 
              sections: [
                 {
-                  title: 'HATS',
+                  title: 'CATS',
                   imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                   id: 1,
                   linkUrl: 'shop/hats'
