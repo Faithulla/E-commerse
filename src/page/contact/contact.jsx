31cf25540a492cf5b51dd0c8fb8d1bab82cc41a9
Faithulla <fayzullashakirov01@gmail.com>
2021-08-14 23:24:10 +0500

    added dummy text to conntact

--- src/page/contact/contact.jsx
+++ src/page/contact/contact.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
-import './contact.scss'
+import React from "react";
+import "./contact.scss";
 const Contact = () => {
-    return ( <div className="contact">
-        <div className="logo"><h1>Contact with us</h1><span>FOR BETTER EXPERIENCE</span></div>
-        <div className="header"></div>
-        <div className="content"></div>
-    </div> );
-}
- 
+  return (
+    <div className="contact">
+      <div className="logo">
+        <h1>Contact with us</h1>
+        <span>FOR BETTER EXPERIENCE</span>
+      </div>
+      <div className="header">
+        <h2>We won`t let you wait</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. At nemo ipsam
+          dolores inventore dolorem. Quis aperiam cum officia eos praesentium
+          rerum maxime veniam deserunt alias, ea, optio omnis nisi quos itaque!
+          Mollitia quo eaque, nostrum aliquam eum id eligendi quisquam
+          molestiae, soluta placeat voluptate ab tenetur repellat optio in? Rem.
+        </p>
+      </div>
+      <div className="content"></div>
+    </div>
+  );
+};
+
 export default Contact;
