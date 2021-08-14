99d7e21f6a1c516928e981821f76371af8ec6155
Faithulla <fayzullashakirov01@gmail.com>
2021-08-14 23:56:52 +0500

    added feedback area

--- src/page/contact/contact.jsx
+++ src/page/contact/contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
         <div className="heade">
         <h2>We won`t let you wait</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At nemo ipsam dolores inventore dolorem. Quis aperiam cum officia eos praesentium rerum maxime veniam deserunt alias, ea, optio omnis nisi quos itaque! Mollitia quo eaque, nostrum aliquam eum id eligendi quisquam molestiae, soluta placeat voluptate ab tenetur repellat optio in? Rem.</p></div>
-        <div className="content"></div>
+        <div className="content"><textarea name="" id="" cols="75" rows="10"></textarea></div>
     </div> );
 }
  
